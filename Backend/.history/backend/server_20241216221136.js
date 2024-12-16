c92f34a0d7f83701adf3ca05045f754051a24517
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-12-16 22:19:17 +0530

    ADD: 🔥 BACKEND FOR CHATAPP

---
+++ Backend/.history/backend/server_20241216221136.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    response: "wellcome to chatapp ",
+  });
+});
+
+
+
+const PORT=process
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ Backend/.history/backend/server_20241216221138.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    response: "wellcome to chatapp ",
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
