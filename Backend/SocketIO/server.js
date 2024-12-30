2c5b599bc5cc70cb29be833ef69a6591b4615b60
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-12-30 20:53:41 +0530

    added socket address

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: "*",
+        origin: "https://chatapp-mern-vvn5.onrender.com",
         methods: ["GET", "POST", "PUT", "DELETE"],
     },
 });
